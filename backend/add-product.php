0942a48d5bd2ede4f3817fc392935f6685334b51
mdmathewdc <mathewdony007@gmail.com>
2021-05-15 18:46:45 +1000

    No changes

---
+++ backend/add-product.php
@@ -0,0 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+        $user1 = "response";
+
+
+echo $user1;
+
+
+?>
--- js/script.js
+++ js/script.js
@@ -14,39 +14,19 @@ $(function() {
 
 
     function addProduct(productName,productDescription,vendorName,productCategory) {
+     
+      $.ajax({
+        type: "POST",
+        url: 'backend/add-product.php',
+        data: {name: 'John'},
+        success: function(data){
+          console.log("Response :" + data);
+        },
+        error: function(xhr, status, error){
+          console.error(xhr);
+        }
+      });
 
-        console.log("Add product function...");
-        var settings = {
-            "url": "https://5e0c9d0ee789975147ebd70d5d71cb1f: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@beerco-pty-ltd.myshopify.com/admin/api/2020-10/products.json",
-            "method": "POST",
-            "timeout": 0,
-            "headers": {
-              "Content-Type": "application/json",
-              "Authorization": "Basic NWUwYzlkMGVlNzg5OTc1MTQ3ZWJkNzBkNWQ3MWNiMWY6c2hwcGFfMmQ4ZTdkMDRjOTFiYzk2OWJjZGU3NjRkMjUyYjA2ODY="
-            },
-            "data": JSON.stringify({
-              "product": {
-                "title": "xxx product",
-                "body_html": "<strong>This is a test product description</strong>",
-                "vendor": "crsip",
-                "product_type": "Barrel hfjdbj",
-                "tags": [
-                  "barrel1",
-                  "barrel2"
-                ],
-                "status": "draft"
-              }
-            }),
-            "dataType": "JSON",
-            "crossDomain": "true"
-
-          };
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-        
-    }
-
+   }
 
 });
