6a3ba1f560fde1358661fc644ace8df2552b4f61
mdmathewdc <mathewdony007@gmail.com>
2021-05-18 10:53:42 +1000

    Added product cards in dashboard

--- js/add-product.js
+++ js/add-product.js
@@ -1,4 +1,5 @@
 $(function() {
+
     console.log("Script.js has been called!");
 
     $("#add-product").click(function()   {
@@ -22,13 +23,25 @@ $(function() {
         data: {'productName': productName, 
                'productDescription': productDescription, 
                'vendorName': vendorName,
-                'productCategory': productCategory
+               'productCategory': productCategory
               },
         success: function(data){
           console.log(JSON.parse(data));
+          $.toast({
+            heading: 'Success',
+            text: 'Product has been added!',
+            showHideTransition: 'fade',
+            icon: 'success'
+        });
+
         },
         error: function(xhr, status, error){
           console.error(xhr);
+          $.toast({
+            heading: 'Error',
+            text: 'Unexpected error!',
+            icon: 'error'
+        })
         }
       });
 
